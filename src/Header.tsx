63bd792dd7449a01453f806202c527e988546ca2
ryanchenstats <ryanchenitis@gmail.com>
2024-11-30 22:48:30 -0600

    add header test

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>My Application Header</h1>
+        </header>
+    );
+};
+
+export default Header;
