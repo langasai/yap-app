d7ce31a84236ec83946b866a3415740bbd618aa5
langasai <contact.langas.ai@gmail.com>
2024-11-30 22:59:56 -0600

    header css

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header>
-            <h1>My Application Header</h1>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#faq">FAQ</a></li>
+                </ul>
+            </nav>
         </header>
     );
 };
