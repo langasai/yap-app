61ec50a8efc863a16fe7baf8d6e79f68976a375b
langasai <contact.langas.ai@gmail.com>
2024-11-30 22:57:20 -0600

    header

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import Header from './header.tsx';
+import Header from './Header.tsx';
 import './index.css'
 
 
