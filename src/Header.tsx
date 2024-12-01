5cccd1267cd40b1accbd3bd24468b6987d889c8f
langasai <contact.langas.ai@gmail.com>
2024-11-30 23:09:23 -0600

    header

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header>
-            <h1>Yap App - Immersion without leaving home.</h1>
+            <h1>Immersion without leaving home.</h1>
         </header>
     );
 };
