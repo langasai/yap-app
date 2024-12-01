bb2fae9e84c42161f3c469847e997708bf1d55cc
langasai <contact.langas.ai@gmail.com>
2024-11-30 23:04:55 -0600

    navbar

---
+++ src/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css'; // {{ edit_1 }} - Optional: for styling
+
+const NavBar: React.FC = () => {
+    return (
+        <div className="navbar">
+            <h1>My Application Header</h1>
+            <nav> {/* {{ edit_2 }} */}
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
