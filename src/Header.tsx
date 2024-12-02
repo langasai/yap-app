5c63f7aec661b746910fb7315330273b4520153c
langasai <contact.langas.ai@gmail.com>
2024-12-01 23:54:26 -0600

    logo

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import logo from './assets/yap.svg'
 const Header: React.FC = () => {
     return (
         <header>
             <h1>Immersion without leaving home.</h1>
+            <img src={logo} alt="logo" style={{ width: '96px', height: '96px' }} />
         </header>
     );
 };
