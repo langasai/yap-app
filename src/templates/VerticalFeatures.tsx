72bff09dade53b95f811b23c835de3d322677f1d
langasai <contact.langas.ai@gmail.com>
2024-12-02 22:45:10 -0600

    wording

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,25 +3,25 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title='Suffering from "100+ day streak on Duolingo?"'
+    title='Suffering from "100+ Day-Streak Syndrome on Duolingo?"'
     description="Most platforms treat language learning as a task/chore/game in your free time. We treat it as if you moved to that language-speaking country."
   >
     <VerticalFeatureRow
       title="Ditch the reading. Embrace the listening."
-      description="Apps have inflated the importance of reading as you get started. The first step to changing the way you think is making the 'uncomfortable' listening comfortable."
+      description="Apps have inflated the importance of reading as you get started. The result is a bunch of langauge learners who can read a little, and cannot speak or understand the langauge. The first step to fluency is making the 'uncomfortable' listening comfortable."
       image="/assets/images/feature1.png"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
       title="Adapt your learning to your life."
-      description="Most learning apps make you jump through countless lessons that do not pertain to you. Let's start learning what you can directly apply to your life right away."
+      description="Most learning apps make you jump through countless lessons that do not pertain to you. Let's start learning what you can directly apply to your life right away. Your language. Your choice. Your time."
       image="/assets/images/feature2.png"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
       title="Change the inner voice."
-      description="Don't learn to translate from your mother tongue. Embrace your different self as you speak a different inner language."
+      description="Don't just learn to translate from your mother tongue like the other language apps make you do. Embrace your different self as you speak a different inner language."
       image="/assets/images/feature3.png"
       imageAlt="Third feature alt text"
     />
