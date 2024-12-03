ae24acd4accaadc269d6dd57764bd4701bc3053e
langasai <contact.langas.ai@gmail.com>
2024-12-02 21:29:50 -0600

    hero layou

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,15 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="hero">
+    <div className="hero">
+      <div className="hero_text" style={{ float: 'left', width: '50%' }}>
+        <h1>Yap App</h1>
+        <h3>Tired of pointless drills and endless vocabulary lists? Yap is the solution. We will help you start speaking in no time.</h3>
+      </div>
+      <div className="hero_img" style={{ float: 'right', width: '50%' }}>
         <img src={hero} alt="hero" />
       </div>
-    </>
+    </div>
   )
 }
 
